ff4f44b50d32f88cdaee86a97f9639e15518bf39
Nipun Sud <nipunsud123@gmail.com>
2020-03-25 20:15:12 -0400

    user sign up redirect

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,15 +26,12 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-    respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
+        session[:user_id] = @user.id
+        redirect_to root_path
       else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+        redirect_to signup_path
       end
-    end
   end
 
   # PATCH/PUT /users/1
