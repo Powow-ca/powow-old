e4fa2ab13530cf1de84fe99d7cf377b9a5b52178
Nipun Sud <nipunsud123@gmail.com>
2020-03-28 14:12:37 -0400

    adding seed db and how it works based on feedback

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# services  = Service.new([{ category: Service.categories[:Consultation]}, { category: Service.categories[:Cooking]}, { category: Service.categories[:Tutoring]}, { category: Service.categories[:Music]}, { category: Service.categories[:Coaching]},  { category: Service.categories[:Languages]}])
+
+Service.where( category: Service.categories[:Consultation]).first_or_create
+Service.where( category: Service.categories[:Cooking]).first_or_create
+Service.where( category: Service.categories[:Tutoring]).first_or_create
+Service.where( category: Service.categories[:Music]).first_or_create
+Service.where( category: Service.categories[:Coaching]).first_or_create
+Service.where( category: Service.categories[:Languages]).first_or_create
