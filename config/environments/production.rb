cc8811713073323eaa7774ee6e6a707c1b72dfe1
Nipun Sud <nipunsud123@gmail.com>
2020-04-11 23:59:34 -0400

    updating domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { :host => 'portal.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'www.powow.ca' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       :address              => 'smtp.gmail.com',
