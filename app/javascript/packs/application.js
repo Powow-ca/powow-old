aa1eb7a10f51a2bf6790d70f7d23651adde65605
Nipun Sud <nipunsud123@gmail.com>
2020-04-23 23:36:44 -0400

    owl

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("packs/carousel");
+require("carousel");
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -16,5 +16,5 @@ require("packs/carousel");
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 import 'bootstrap'
-import 'owl.carousel'
-import 'owl.carousel/dist/assets/owl.carousel.css'
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel';
