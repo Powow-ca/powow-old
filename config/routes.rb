16896839e657548b966b4df9cda12ecff4a070fc
Celso Soares <celso@tangivel.com>
2020-06-28 22:50:17 +0100

    Create new page 'aboutus' and add basic html

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
 
+  get 'about', to: 'aboutus#index'
+
   get 'pro', to: 'users#pro' 
   post 'feedback', to: 'feedback#create', as: 'feedback'
 
